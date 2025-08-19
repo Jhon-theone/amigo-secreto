2cb38ed8112eae9e393a73e26e0a9d78180ba6c4
jhon Rodríguez <jhonalexcortes31@gmail.com>
2025-08-19 15:37:02 -0500

    array para almacenar nombres

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Array para almacenar los nombres.
 let amigos = [];
 
 function nombreDeAmigos () {
