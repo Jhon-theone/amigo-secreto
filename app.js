149a24d110a78d12590c5084a7b4b1c4766fb2d2
jhon Rodríguez <jhonalexcortes31@gmail.com>
2025-08-19 16:05:26 -0500

    funcion para agregar amigos

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 // Array para almacenar los nombres.
 let amigos = [];
 
-function nombreDeAmigos () {
-    document.getElementById('amigo')
+// Funcion para agregar amigos.
+function agregarAmigo () {
+
+    // Captuarar el valor de campo de entrada.
+    const nombre = document.getElementById('amigo').value.trim();
+
+    // Validar la entrada.
+    if (nombre == "") {
+        alert("Por favor inserte un nombre");
+        return;
+    }
+
+    // Actualizar el array de amigos.
+    amigos.push(nombre);
+    console.log(amigos);
+    // Limpiar el campo de entrada.
+    document.getElementById('amigo').value = "";
 }
